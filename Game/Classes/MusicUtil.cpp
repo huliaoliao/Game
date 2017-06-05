067b0cd7922b0c60430f10fe14527b9248c11972
huliaoliao <syzxhyw@126.com>
2017-06-05 22:37:33 +0800

    添加： 封装音乐播放

---
+++ Game/Classes/MusicUtil.cpp
@@ -0,0 +1,142 @@
+﻿#include "Musicutil.h"
+#include "audio/include/AudioEngine.h"
+#include "audio/include/SimpleAudioEngine.h"
+using namespace std;
+USING_NS_CC;
+using namespace CocosDenshion;
+
+namespace util 
+{
+	// 保存/读取音乐设置的key
+	const char *const kMusicKey = "kMusicKey";
+	const char *const kEffectKey = "kEffectKey";
+	// 保存/读取音量的key
+	const char *const kEffectVolumeKey = "kEffectVolumeKey";
+	const char *const kBackgroundMusicVolumeKey = "kBackgroundMusicVolumeKey";
+
+	bool MusicUtil::is_bg_music_on_ = true;
+	bool MusicUtil::is_effect_music_on_ = true;
+	bool MusicUtil::is_playing_bg = false;
+	int MusicUtil::m_nMusicID = 0;
+	float MusicUtil::m_fEffectVolume = 1.0f;
+	float MusicUtil::m_fBackgroundMusicVolume = 1.0f;
+	string MusicUtil::bg_music_name_ = "";
+
+	void MusicUtil::DoInit()
+	{
+		// 读取音乐音效设置
+		is_bg_music_on_ = UserDefault::getInstance()->getBoolForKey(kMusicKey, false);
+		is_effect_music_on_ = UserDefault::getInstance()->getBoolForKey(kEffectKey, true);
+		// 读取音量
+		m_fEffectVolume = UserDefault::getInstance()->getFloatForKey(kEffectVolumeKey, 1.0f);
+		m_fBackgroundMusicVolume = UserDefault::getInstance()->getFloatForKey(kBackgroundMusicVolumeKey, 1.0f);
+	}
+
+	void MusicUtil::PlayBackgroundMusic(const char *music_name, bool is_loop)
+	{
+		if (is_playing_bg)
+		{
+			return;
+		}
+		bg_music_name_ = music_name;
+		if (is_bg_music_on_)
+		{
+			is_playing_bg = true;
+			SimpleAudioEngine::getInstance()->playBackgroundMusic(music_name, true);
+		}
+	}
+
+	void MusicUtil::PauseBackgroundMusic()
+	{
+		if (is_bg_music_on_)
+		{
+			//experimental::AudioEngine::pause(m_nMusicID);
+			SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+		}
+	}
+
+	void MusicUtil::ResumeBackgroundMusic()
+	{
+		if (is_playing_bg)
+		{
+			if (is_bg_music_on_)
+			{
+				//experimental::AudioEngine::resume(m_nMusicID);
+				SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+			}
+		}
+	}
+
+	void MusicUtil::StopBackgroundMusic()
+	{
+		is_playing_bg = false;
+		//experimental::AudioEngine::stop(m_nMusicID);
+		SimpleAudioEngine::getInstance()->stopBackgroundMusic();
+	}
+
+	void MusicUtil::PlayEffectMusic(const char *effect_name)
+	{
+		//	if (is_effect_music_on_)
+		//{
+		//experimental::AudioEngine::play2d(effect_name, false, m_fEffectVolume);
+		SimpleAudioEngine::getInstance()->playEffect(effect_name);
+		//	}
+	}
+
+	void MusicUtil::EnterForeground()
+	{
+		if (is_bg_music_on_)
+		{
+			//experimental::AudioEngine::resume(m_nMusicID);
+			SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+		}
+	}
+
+	void MusicUtil::SaveSettings()
+	{
+		UserDefault::getInstance()->setBoolForKey(kMusicKey, is_bg_music_on_);
+		UserDefault::getInstance()->setBoolForKey(kEffectKey, is_effect_music_on_);
+		UserDefault::getInstance()->flush();
+	}
+
+	void MusicUtil::End()
+	{
+		SaveSettings();  // 游戏结束前保存一次设置，以免忘了游戏中忘记保存了
+		//experimental::AudioEngine::end();
+		SimpleAudioEngine::getInstance()->end();
+	}
+
+	void MusicUtil::stopAll()
+	{
+		//experimental::AudioEngine::stopAll();
+	}
+
+	void MusicUtil::setVolume(float fVolume)
+	{
+		log("MusicUtil::setVolume %f", fVolume);
+		m_fEffectVolume = fVolume;
+		SimpleAudioEngine::getInstance()->setEffectsVolume(m_fEffectVolume);
+		UserDefault::getInstance()->setFloatForKey(kEffectVolumeKey, m_fEffectVolume);
+		UserDefault::getInstance()->flush();
+	}
+
+	float MusicUtil::getVolume()
+	{
+		return m_fEffectVolume;
+	}
+
+	void MusicUtil::setBgMusicVolume(float fVolume)
+	{
+		log("MusicUtil::setBgMusicVolume %f", fVolume);
+		m_fBackgroundMusicVolume = fVolume;
+		//experimental::AudioEngine::setVolume(m_nMusicID, m_fBackgroundMusicVolume);
+		SimpleAudioEngine::getInstance()->setBackgroundMusicVolume(m_fBackgroundMusicVolume);
+		UserDefault::getInstance()->setFloatForKey(kBackgroundMusicVolumeKey, m_fBackgroundMusicVolume);
+		UserDefault::getInstance()->flush();
+	}
+
+	float MusicUtil::getBgMusicVolume()
+	{
+		return m_fBackgroundMusicVolume;
+	}
+}
---
+++ Game/Classes/MusicUtil.h
@@ -0,0 +1,89 @@
+﻿/*******************************************************************************
+处理音乐的类
+若用此类管理音乐，需要：
+1. 在开始游戏时（比如splash界面）调用MusicUtil::DoInit()进行初始化
+2. 在AppDelegate::applicationDidEnterBackground中调用MusicUtil::PauseBackgroundMusic暂停背景音乐
+3. 在AppDelegate::applicationWillEnterForeground中调用MusicUtil::EnterForeground恢复背景音乐
+4. 在AppDelegate::~AppDelegate中调用MusicUtil::End结束音乐
+5. 在游戏要退出时会自动保存当前的设置。如果想要手动保存设置（比如玩家切换了音乐或音效的开启就想立即保存），
+   调用MusicUtil::SaveSettings。
+********************************************************************************/
+
+#ifndef __UTIL_MUSICUTIL_H__
+#define __UTIL_MUSICUTIL_H__
+
+#include "cocos2d.h"
+
+namespace util {
+
+class MusicUtil {
+public:
+	// 从文件中读取音乐设置
+	static void DoInit();
+
+	// 背景音乐是否开了
+	static bool is_background_music_on() { return is_bg_music_on_; }
+
+	// 设置背景音乐开关
+	static void set_is_background_music_on(bool is_on) { is_bg_music_on_ = is_on; }
+
+	// 特效声音是否开了
+	static bool is_effect_music_on() { return is_effect_music_on_; }
+
+	// 设置特效声音开关
+	static void set_is_effect_music_on(bool is_on) { is_effect_music_on_ = is_on; }
+
+	// 播放背景音乐
+	 // bg_music_name: 背景音乐名字
+	// is_loop: 是否循环播放
+	static void PlayBackgroundMusic(const char *music_name, bool is_loop);
+
+	// 暂停
+	static void PauseBackgroundMusic();
+
+	 // 恢复
+	static void ResumeBackgroundMusic();
+
+	// 停止
+	static void StopBackgroundMusic();
+
+	 // 播放特效声音，music_name为特效声音名字
+	static void PlayEffectMusic(const char *effect_name);
+
+	// 游戏进入前台
+	static void EnterForeground();
+
+	 // 保存设置到文件
+	static void SaveSettings();
+
+	// 结束音乐播放（在游戏将要退出时调用）
+	static void End();
+
+	// 停止所有声音
+	static void stopAll();
+
+	// 设置并保存音效音量
+	static void setVolume(float fVolume);
+
+	// 获得保存的音效音量
+	static float getVolume();
+
+	// 设置并保存背景音乐音量
+	static void setBgMusicVolume(float fVolume);
+
+	 // 获得保存的背景音乐音量
+	static float getBgMusicVolume();
+
+private:
+	static bool	is_bg_music_on_;			// 背景音乐是否开了
+	static bool	is_effect_music_on_;		// 特效声音是否开了
+	static bool	is_playing_bg;				// 是否正在播放背景音乐
+	static int	m_nMusicID;					// 正在播放的背景音乐的id
+	static float	m_fEffectVolume;			// 音效音量
+	static float	m_fBackgroundMusicVolume;	// 背景音乐音量
+	static std::string bg_music_name_;		// 上次播放的背景音乐的名称
+};
+
+}
+
+#endif // UTIL_MUSICUTIL_H_
