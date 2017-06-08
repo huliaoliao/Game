2cd3e56143632acdfac4d9f777a0f3d65460b672
huliaoliao <syzxhyw@126.com>
2017-06-09 00:28:17 +0800

    添加： 玩家头像层

---
+++ Game/Classes/HeadImageLayer.cpp
@@ -0,0 +1,70 @@
+#include "HeadImageLayer.h"
+
+#include "Common.h"
+#include "HeadImage.h"
+
+static const std::string layerName = "headImageLayer";
+
+void HeadImageLayer::createLayer()
+{
+	auto layer = HeadImageLayer::create();
+	if (cocos2d::Director::getInstance()->getRunningScene()->getChildByName(layerName))
+	{
+		cocos2d::Director::getInstance()->getRunningScene()->removeChildByName(layerName);
+	}
+	cocos2d::Director::getInstance()->getRunningScene()->addChild(layer, COMMON_DIALOG_ZORDER, layerName);
+}
+
+void HeadImageLayer::destroyLayer()
+{
+	if (cocos2d::Director::getInstance()->getRunningScene()->getChildByName(layerName))
+	{
+		cocos2d::Director::getInstance()->getRunningScene()->removeChildByName(layerName);
+	}
+}
+
+bool HeadImageLayer::init()
+{
+	if (!cocos2d::Layer::init())
+	{
+		return false;
+	}
+
+	if (initView() == false)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool HeadImageLayer::initView()
+{
+	auto winSize = cocos2d::Director::getInstance()->getWinSize();
+
+	//电脑1的位置（右上方）
+	_computerOneHeadImage = HeadImage::create();
+	_computerOneHeadImage->setOrientation(HeadImage::RIGHT);
+	_computerOneHeadImage->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
+	_computerOneHeadImage->setPosition(
+		cocos2d::Point(winSize.width * 0.9, winSize.height * 0.65));
+	this->addChild(_computerOneHeadImage, 1);
+
+	//电脑2的位置（左上方）
+	_computerTwoHeadImage = HeadImage::create();
+	_computerTwoHeadImage->setOrientation(HeadImage::LEFT);
+	_computerTwoHeadImage->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
+	_computerTwoHeadImage->setPosition(
+		cocos2d::Point(winSize.width * 0.1, winSize.height * 0.65));
+	this->addChild(_computerTwoHeadImage);
+
+	//手动玩家的位置
+	_holdPlayerHeadImage = HeadImage::create();
+	_holdPlayerHeadImage->setOrientation(HeadImage::LEFT);
+	_holdPlayerHeadImage->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
+	_holdPlayerHeadImage->setPosition(
+		cocos2d::Point(winSize.width * 0.1, winSize.height * 0.4));
+	this->addChild(_holdPlayerHeadImage);
+
+	return true;
+}
---
+++ Game/Classes/HeadImageLayer.h
@@ -0,0 +1,32 @@
+#ifndef					__HEADIMAGE_LAYER_H__
+#define					__HEADIMAGE_LAYER_H__
+
+/*	玩家层：存放玩家头像信息	---	2017.6.7 
+*/
+
+#include "cocos2d.h"
+
+class HeadImage;
+
+class HeadImageLayer : public cocos2d::Layer
+{
+public:
+	static void createLayer();
+
+	static void destroyLayer();
+
+private:
+	CREATE_FUNC(HeadImageLayer);
+
+	virtual bool init() override;
+
+private:
+	bool initView();
+
+private:
+	HeadImage* _computerOneHeadImage;
+	HeadImage* _computerTwoHeadImage;
+	HeadImage* _holdPlayerHeadImage;
+};
+
+#endif
